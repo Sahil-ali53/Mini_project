3b697f68ba385446c5634df999d38fff9e0ac486
sahil ali <sahilshahadatali786@gmail.com>
2026-01-21 14:53:32 +0530

    SearchList

---
+++ SearchList/index.js
@@ -0,0 +1,29 @@
+// const { Profiler } = require("react");
+let userContainer = document.querySelector('.userContainer')
+
+let searchInput = document.querySelector("#searchInput");
+
+//user array
+const arr = [
+    {
+        ProfilerUrl: 'https://images.squarespace-cdn.com/content/v1/5d2346cbe7875300014eeb79/1674738312554-9NCZQV6OIE9AU0FW4F5T/fda507cd073a9fb801943590aa9620ac--male-models-dame.jpg" alt="image is loading',
+        name: 'Nikhil',
+        email: "Nikhilkhan78@gmail.com",
+
+
+    },
+    {
+        ProfilerUrl: 'https://images.squarespace-cdn.com/content/v1/5d2346cbe7875300014eeb79/1674738312554-9NCZQV6OIE9AU0FW4F5T/fda507cd073a9fb801943590aa9620ac--male-models-dame.jpg" alt="image is loading',
+        name: 'Roshani',
+        email: "Nikhilkhan78@gmail.com",
+
+
+    },
+    {
+        ProfilerUrl: 'https://images.squarespace-cdn.com/content/v1/5d2346cbe7875300014eeb79/1674738312554-9NCZQV6OIE9AU0FW4F5T/fda507cd073a9fb801943590aa9620ac--male-models-dame.jpg" alt="image is loading',
+        name: 'sana kera',
+        email: "Nikhilkhan78@gmail.com",
+
+
+    },
+]
